d54274715c3c881cbede008479323c288e13d833
matteopaul <matteo.illing@icloud.com>
2019-06-21 13:31:58 +0200

    initial commit

--- gridsome.config.js
+++ gridsome.config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       use: '@gridsome/source-filesystem',
       options: {
-        path: 'posts/*.md',
+        path: 'posts/*.json',
         typeName: 'Post',
         remark: {
           plugins: [
